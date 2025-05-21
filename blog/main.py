e6565d8888a6d726875493182856f686060c9948
hammad-ali-next <hammad.ali@visnext.net>
2025-05-21 13:45:55 +0500

    changed root path

--- blog/main.py
+++ blog/main.py
@@ -10,6 +10,12 @@ from .routers import blogs_routes, users_routes, auth_routes
 
 app = FastAPI()
 
+@app.get('/')
+def root():
+    return {'detail' : {'deploy' : 'successfully', 
+                         'Visit' : '"https://fast-api-first.vercel.app/docs"'}}
+
+
 models.Base.metadata.create_all(bind=engine)
 
 app.include_router(auth_routes.router)
