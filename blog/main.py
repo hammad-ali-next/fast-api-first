eace73fece3dbc4b7d20b2d0aa40cbb8ac411a3f
Hammad Ali <212019577+hammad-ali-next@users.noreply.github.com>
2025-06-03 02:18:47 +0500

    added one more orgin

--- blog/main.py
+++ blog/main.py
@@ -13,7 +13,8 @@ app = FastAPI()
 
 origins = [
     "http://localhost:3000",  # <-- your frontend origin exactly
-    "http://127.0.0.1:3000"
+    "http://127.0.0.1:3000",
+    "https://my-blog-app.vercel.app/"
     # add more if needed
 ]
 
